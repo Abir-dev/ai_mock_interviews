429431fb6f552accc6e9a69b230367d64adb38ba
Abir-dev <abirlalbanerjee44@gmail.com>
2025-04-09 11:06:52 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,10 @@ import { toast } from "sonner"
 import FormField from "./FormField"
 import { Form } from "./ui/form"
 import { useRouter } from "next/navigation"
+import { create } from "domain"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/firebase/client"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -40,18 +44,48 @@ const AuthForm = ({ type }: { type: FormType }) => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
         if(type  === 'sign-up') {
+          const { email, password, name } = values;
+
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          const result  = await signUp({
+            uid: userCredential.user.uid,
+            name: name!,
+            email,
+            password,
+          })
+
+          if(!result?.success) {
+            toast.error(result?.message);
+            return;
+          }
+
           toast.success("Account created successfully. Please sign in.")
           router.push('/sign-in')
           console.log("SIGN UP", values)
         } else {
+          const { email, password } = values;
+          const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+          const idToken = await userCredential.user.getIdToken();
+
+          if(!idToken) {
+            toast.error("Sign in failed")
+            return;
+          }
+          await signIn({
+            email,
+            idToken,
+          })
+
           toast.success("Signed in successfully.")
           router.push('/')
           console.log("SIGN IN", values)
         }
     } catch (error) {
+        console.log(error);
         toast.error(`There was an error: ${error}`)
     }
   }
